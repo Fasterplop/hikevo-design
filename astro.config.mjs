16f4c87fc54543bfd371a664d837464c4a2406e2
Luis <fasterplop@gmail.com>
2025-12-18 17:54:55 -0400

    sitemap and robots added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare'; // [!code ++]
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://hikevodesign.com', 
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   adapter: cloudflare(), // [!code ++] (Conecta el adaptador)
 });
