c19ce7aaaac0a1a39b6bf6bcb496bc3e6b565797
Luis <fasterplop@gmail.com>
2025-12-28 12:42:09 -0400

    i18n added, but needs review

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://hikevodesign.com',
+  output: 'static',
   // Configuración de i18n
   i18n: {
     defaultLocale: "es", // Idioma principal
